37fdf62b4e6b4b6b253cea6400ca3bd61fb2262a
Ricardo Peralta <ricardo.peralta@alumnos.udg.mx>
2018-10-25 00:21:40 -0500

    missing letter

--- mail/index.js
+++ mail/index.js
@@ -5,7 +5,7 @@ class Mailer {
     this.transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
-      secureConnection: proces.env.MAIL_SECURE,
+      secureConnection: process.env.MAIL_SECURE,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
