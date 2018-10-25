a3ad217d429855fee1d9e0f5b9496dbdea649476
Ricardo Peralta <ricardo.peralta@alumnos.udg.mx>
2018-10-25 00:07:00 -0500

    testing .env

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Hello from ${process.env.PORT || 'default port'}`);
 });
 
 module.exports = router;
